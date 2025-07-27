e9f6d764a58f47033f6803c9093d514f29d4d5e3
SaliAbdullaeva <imansheyh@mail.ru>
2025-07-27 21:33:56 +0300

    Initial commit

---
+++ src/test/java/com/example/kamalotus/KamalotusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kamalotus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamalotusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
